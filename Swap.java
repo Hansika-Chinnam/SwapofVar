5094da9389df5f29928329cc2042dab5c5f578ce
CHINNAM HANSIKA PATEL <hansikachinnam12@gmail.com>
2025-01-28 15:51:58 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,18 @@
+
+ public class Swap {
+    public static void main(String[] args){
+       int temp;
+       int x=2 ,y=3;
+       System.out.println("Before Swaping:");
+       System.out.println(" x="+x);
+       System.out.println(" y="+y);
+       temp=x;
+       x=y;
+       y=temp;
+       System.out.println("After swaping:");
+       System.out.println(" x="+x);
+       System.out.println(" y="+y);
+       
+
+    }
+}
